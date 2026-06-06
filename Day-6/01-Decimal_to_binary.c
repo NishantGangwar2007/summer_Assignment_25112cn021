16a95e9dcc5cd83295ae13b979105dd4bc7aeeb9
NishantGangwar2007 <nishantgangwar9548@gmail.com>
2026-06-06 16:56:26 +0530

    Day-6 assignment uploaded

---
+++ Day-6/01-Decimal_to_binary.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(){
+    int n,bin[32],i=0;
+    printf("Enter a decimal number: ");
+    scanf("%d",&n);
+
+    while(n>0){
+        bin[i] = n%2;
+        n= n/2;
+        i++;}
+    printf("Binary = ");
+    for (int j=i-1;j>=0; j--) {
+        printf("%d", bin[j]);
+    }
+    return 0;
+}
