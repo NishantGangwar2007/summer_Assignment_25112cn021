d3233547f211c6973e11fc913cc4afe583e98385
NishantGangwar2007 <nishantgangwar9548@gmail.com>
2026-06-15 17:29:57 +0530

    Day-15 assignment uploaded

---
+++ Day-15/01-reverse_array.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+int a[50],n,i;
+printf("Enter size of array: ");
+scanf("%d",&n);
+printf("Enter array elements:\n");
+for(i=0;i<n;i++){
+scanf("%d",&a[i]);
+}
+printf("Reversed array: ");
+for(i=n-1;i>=0;i--){
+printf("%d ",a[i]);
+}
+return 0;
+}
